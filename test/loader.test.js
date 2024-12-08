e8935bbd56a3aedd8160dc6b3a4939da25000385
Riceball LEE <snowyu.lee@gmail.com>
2024-12-08 16:04:03 +0800

    test: import()

--- test/loader.test.js
+++ test/loader.test.js
@@ -1,4 +1,9 @@
 import assert from 'assert'
+
+// import '../dist/index.js' // only for import() function
+// const echo = await import('./echo.zip').then(m => m.echo)
+// const add = await import('./echo.zip#add.js').then(m => m.add)
+
 import { echo } from './echo.zip'
 import { add } from './echo.zip#add.js'
 
