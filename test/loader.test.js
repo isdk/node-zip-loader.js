b6aa1b4bc4f0670917fe49e7f83c96cfffbb1513
Riceball LEE <snowyu.lee@gmail.com>
2024-12-13 12:14:51 +0800

    test: vitest buggy

--- test/loader.test.js
+++ test/loader.test.js
@@ -1,12 +1,24 @@
 import assert from 'assert'
 
-// import '../dist/index.js' // only for import() function
-// const echo = await import('./echo.zip', {with: {a: "1"}}).then(m => m.echo)
-// const add = await import('./echo.zip#add.js').then(m => m.add)
+// `/* @vite-ignore */` can not work.
+/*
+import '../dist/index.js' // only for import() function
+describe('zip loader', ()=>{
+  it('should work', async ()=>{
+    const echo = await import('./echo.zip', {with: {a: "1"}}).then(m => m.echo)
+    const add = await import('./echo.zip#add.js').then(m => m.add)
+    assert.deepStrictEqual(await echo(1, 2, 3), [1, 2, 3])
+    assert.equal(add(2,3), 5)
+  })
+})
+//*/
 
+
+//*
 import { echo } from 'zip:./echo.zip' with {a: "1"}
 import { add } from 'zip:./echo.zip#add.js'
 
 assert.deepStrictEqual(await echo(1, 2, 3), [1, 2, 3])
 assert.equal(add(2,3), 5)
 console.log('test passed')
+//*/
